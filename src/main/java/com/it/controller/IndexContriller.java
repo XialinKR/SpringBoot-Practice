a6b4659cd05e227a544fafcbbcff6a1d0fda5824
xialinKR <1034138273@qq.com>
2019-05-05 16:15:00 +0800

    练习进阶

--- src/main/java/com/it/controller/IndexContriller.java
+++ src/main/java/com/it/controller/IndexContriller.java
@@ -1,6 +1,7 @@
 package com.it.controller;
 
 import com.it.entity.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,12 @@ import java.util.List;
 @RestController
 public class IndexContriller {
 
+    @Value("${com.it.username}")
+    private String username;
+
+    @Value("${com.it.password}")
+    private String password;
+
     @GetMapping("/Index")
     public List<User> getUser(){
         List<User> list = new ArrayList<>();
@@ -18,6 +25,12 @@ public class IndexContriller {
         user.setUsername("郭采洁");
         user.setPassword("123");
         list.add(user);
+        System.out.println(username+password);
         return list;
+    }
+
+    @GetMapping("/username")
+    public String getUsername(){
+        return username;
     }
 }
